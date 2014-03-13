4b1ba81f8529f7866d5d6b2bbc84077bf003cf3b
Caroline Zhu <Caroline@cs-212-171.cs.ucla.edu>
2014-03-13 13:42:39 -0700

    fixed all errors

--- NATCAR.ino
+++ NATCAR.ino
@@ -1,4 +1,6 @@
+#include <SoftwareServo.h>
 #include <Servo.h>
+
 
 //PWM enabled digital pins: 3,5(not too good),6(not good either),9,10,11,13
 //Analog inputs: A0-A5, A6-A11 (on digital pins: 4,6,7,8,9,10,12)
@@ -9,12 +11,18 @@
 #define centerAFE     A1   //AFE center
 #define rightAFE      A2   //AFE right
 
-//define useful constants
-int i;    //for 'for' loops and such
-int turn_angle;  //used to control speed (PID control)
-int input_AFE;  //ranges from [0, 1024)
+//constant integers
 const int TOLERANCE = 2;    //for little variations in signal
 const int PWM_MAX = 255;
+  // state definitions
+
+//define useful constants
+int i;    //for 'for' loops and such
+int input_AFE;  //ranges from [0, 1024)
+float turn_angle;  //used to control speed (PID control); in radians
+int center;
+int left;
+int right;
 
 SoftwareServo m_servo;  //create servo object
 
@@ -25,8 +33,8 @@ void motor_control(int turn_angle);
 //Setup pins for specific uses
 void setup()
 {
-  pinMode(motorF, OUTPUT);
-  pinMode(motorR, OUTPUT);
+  pinMode(motorDRIVE, OUTPUT);
+  pinMode(motorREVERSE, OUTPUT);
   pinMode(servo, OUTPUT);
   pinMode(leftAFE, INPUT);
   pinMode(centerAFE, INPUT);
@@ -34,19 +42,19 @@ void setup()
   
   m_servo.attach(servo);  //attach servo to servo object
   
-  Serial.being(9600);    //this will be here for monitoring AFE input
+  Serial.begin(9600);    //this will be here for monitoring AFE input
 }
 
 void motor_control(int turn_angle)
 {
   //it's not this simple; this is just an idea :P
-  analogWrite(motor_f, PWM_MAX - 2*turn_angle);
+  analogWrite(motorDRIVE, PWM_MAX - 2*turn_angle);
   
   //FAST STOP
   //analogWrite(motor_f, 0);
 }
 
-void servo_control(int left, int center, int right);    //Servo Example in Arduino info
+void servo_control(int left, int center, int right)
 {
   //map the readings (0 to 1023) to an angle (0 to 179)
   int mapL = map(left, 0, 1023, 0, 179);
